23357674beb87b35292f5a46a0005c1a6579ea6f
Samiylo Kryshu <skryshu@gmail.com>
2020-04-07 19:45:07 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -28,7 +28,7 @@ class Waiter
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
     end
-    best
+    best_tipped_meal.customer
   end
-  best_tipped_meal.customer
+  
 end
